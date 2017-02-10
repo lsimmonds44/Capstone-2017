88ac2b6f3aac438bb5dfb968e8fea0072ea2c86d
EricWCD <EricWNewCom@gmail.com>
2017-02-10 10:38:26 -0600

    Add main window UI

--- SnackOverflowSolution/DataAccessLayer/DBConnection.cs
+++ SnackOverflowSolution/DataAccessLayer/DBConnection.cs
@@ -13,7 +13,8 @@ namespace DataAccessLayer
         {
             // this should be the only place in your 
             // app the connection string  can be found.
-            var connString = @"Data Source=MBE-PC;Initial Catalog=SnackOverflowDB;Integrated Security=True"; // conn string for laptop
+            var connString = @"Data Source=nh229cf;Initial Catalog=SnackOverflowDB;Integrated Security=True"; // conn string for class computer
+            //var connString = @"Data Source=MBE-PC;Initial Catalog=SnackOverflowDB;Integrated Security=True"; // conn string for laptop
 
             // var connString = @"Data Source=WIN-I4H924MDOL3\SQLEXPRESS;Initial Catalog=vehicleBuddyDB;Integrated Security=True"; // conn string for home iMac
             var conn = new SqlConnection(connString);
---
+++ SnackOverflowSolution/LogicLayer/EmployeeManager.cs
@@ -0,0 +1,22 @@
+﻿using DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class EmployeeManager : IEmployeeManager
+    {
+        Employee _employee = null;
+
+        public Employee RetrieveEmployeeByUserName(string userName)
+        {
+            _employee = new Employee();
+            _employee.EmployeeId = 10200;
+
+            return _employee;
+        }
+    } // end class
+} // end namespace
