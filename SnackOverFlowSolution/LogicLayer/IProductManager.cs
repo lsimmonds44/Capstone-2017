b65abb7cfc59a3377ecbb7108b7b6897314dd89e
unknown <nh229u23@NH229C23.csd.bit>
2017-02-24 09:08:41 -0600

    Dan Brown, Adding Product accessor, Interface and manager

---
+++ SnackOverFlowSolution/LogicLayer/IProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public interface IProductManager
+    {
+
+        /// <summary>
+        /// Dan Brown
+        /// Created on 2017/03/10
+        /// 
+        /// Delete a product using the productID
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        int DeleteProduct(int productID);
+
+    }
+}
