3102176a0775aea4ca166f71401a6edeb1f012ba
unknown <nh229u14@NH229C14.csd.bit>
2017-03-24 10:51:03 -0500

    Added "Update Delivery" functionality; fixed delivery tab. - Aaron usher

---
+++ SnackOverFlowSolution/LogicLayer/DeliveryTypeManager.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    /// <summary>
+    /// Aaron Usher
+    /// Created: 2017/03/24
+    /// 
+    /// Class to reprsent a delivery type manager.
+    /// </summary>
+    public class DeliveryTypeManager : IDeliveryTypeManager
+    {
+        /// <summary>
+        /// Aaron Usher
+        /// Created: 2017/03/24
+        /// 
+        /// Retrieves a list of all delivery types.
+        /// </summary>
+        /// <returns>A list of delivery types.</returns>
+        public List<string> RetrieveDeliveryTypeList()
+        {
+            List<string> result = null;
+            try
+            {
+                result = DeliveryTypeAccessor.RetrieveDeliveryTypeList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ SnackOverFlowSolution/LogicLayer/IStatusManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LogicLayer
+{
+    /// <summary>
+    /// Aaron Usher
+    /// Created: 2017/03/24
+    /// 
+    /// Interface to represent a status manager.
+    /// </summary>
+    public interface IStatusManager
+    {
+        List<string> RetrieveStatusList();
+    }
+}
