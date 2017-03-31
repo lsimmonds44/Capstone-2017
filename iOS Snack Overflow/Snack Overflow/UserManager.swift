c85b5490b56b587e982de87d83ffb8802d3f63e8
Eric <MPBR@Erics-MacBook-Pro.local>
2017-03-30 23:37:59 -0500

    Added the start of the snack overflow iOS app

---
+++ iOS Snack Overflow/Snack Overflow/UserManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserManager.swift
+//  Snack Overflow
+//
+//  Created by MBPR on 3/30/17.
+//  Copyright © 2017 Capstone. All rights reserved.
+//
+
+import Foundation
+
+class UserManager {
+    func validateLogin(username:String,password:String, completion: @escaping (_ result:Bool)->())
+    {
+        // not implemented. Need connection to server.
+    }
+}
