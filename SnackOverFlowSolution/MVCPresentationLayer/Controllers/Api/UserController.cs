250264a079e073f56168b345024c4bea665f4071
rforbes136 <robbieforbes96@gmail.com>
2017-04-02 18:29:01 -0500

    Added the start of the web api

---
+++ SnackOverFlowSolution/MVCPresentationLayer/Controllers/Api/UserController.cs
@@ -0,0 +1,46 @@
+﻿using DataObjects;
+using LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCPresentationLayer.Controllers.Api
+{
+    public class UserController : ApiController
+    {
+        IUserManager _userManager = new UserManager();
+
+        /// <summary>
+        /// Robert Forbes
+        /// 2017/04/02
+        /// 
+        /// Api call to try retrirve a user with the entered user name and password
+        /// Returns null if no user was found with the passed in login details
+        /// </summary>
+        /// <param name="userName">The username to search for</param>
+        /// <param name="hash">The has of the password to search for</param>
+        /// <returns>A user matching the passed in username and password or null</returns>
+        [System.Web.Http.HttpGet]
+        public User Login(string userName, string hash)
+        {
+            try
+            {
+                if(_userManager.AuthenticateUser(userName, hash)){
+                    return _userManager.RetrieveUserByUserName(userName);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
