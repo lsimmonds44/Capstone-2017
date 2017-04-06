ff0ad8241a72bc1afd8c4cb059a15212b959fb0d
Christian <christian.lopez.home@gmail.com>
2017-04-06 12:01:31 -0500

    CL Created SupplierWithAgreements for MVC Object Model

---
+++ SnackOverFlowSolution/DataObjects/SupplierWithAgreements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    /// <summary>
+    /// Christian Lopez
+    /// 2017/04/06
+    /// 
+    /// Exists for MVC Modeling
+    /// </summary>
+    public class SupplierWithAgreements : Supplier
+    {
+        public List<Agreement> Agreements { get; set; }
+    }
+}
