a9f4ceaa1a700df10169afea99ec4aba3dc460df
unknown <nh229u21@NH229U21.csd.bit>
2017-04-07 10:58:47 -0500

    Small update to api

--- SnackOverFlowSolution/MVCPresentationLayer/Controllers/Api/UserController.cs
+++ SnackOverFlowSolution/MVCPresentationLayer/Controllers/Api/UserController.cs
@@ -15,20 +15,21 @@ namespace MVCPresentationLayer.Controllers.Api
 
         /// <summary>
         /// Robert Forbes
-        /// 2017/04/02
+        /// 2017/04/07
         /// 
-        /// Api call to try retrirve a user with the entered user name and password
+        /// Api call to try retrieve a user with the entered user name and password
         /// Returns null if no user was found with the passed in login details
         /// </summary>
         /// <param name="userName">The username to search for</param>
-        /// <param name="hash">The has of the password to search for</param>
+        /// <param name="password">The password to search for</param>
         /// <returns>A user matching the passed in username and password or null</returns>
         [System.Web.Http.HttpGet]
-        public User Login(string userName, string hash)
+        public User Login(string userName, string password)
         {
             try
             {
-                if(_userManager.AuthenticateUser(userName, hash)){
+                if (_userManager.AuthenticateUser(userName, password))
+                {
                     return _userManager.RetrieveUserByUserName(userName);
                 }
                 else
