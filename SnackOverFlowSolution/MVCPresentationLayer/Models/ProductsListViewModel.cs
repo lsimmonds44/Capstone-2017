022189ada56da9582331bb4b9dfa60fdea441134
Skyler H <skylerhiscock@gmail.com>
2017-04-08 15:02:33 -0500

    comments

--- SnackOverFlowSolution/MVCPresentationLayer/Models/ProductsListViewModel.cs
+++ SnackOverFlowSolution/MVCPresentationLayer/Models/ProductsListViewModel.cs
@@ -6,7 +6,8 @@ namespace MVCPresentationLayer.Models
     /// <summary>
     /// Created by Michael Takrama
     /// 04/07/2017
-    /// 
+    /// Update by Skyler Hiscock
+    /// 04/08/2017
     /// Composite Data Object View Model for Product/Index
     /// </summary>
 
