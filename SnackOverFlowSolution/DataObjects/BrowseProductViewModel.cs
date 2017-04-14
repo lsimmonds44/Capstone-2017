aa17071410125e0ca4037e6016938c0daa916e26
miketak <mtakrama@yahoo.com>
2017-04-14 11:30:09 -0500

    Cart Integration to BrowseProductViewModel

--- SnackOverFlowSolution/DataObjects/BrowseProductViewModel.cs
+++ SnackOverFlowSolution/DataObjects/BrowseProductViewModel.cs
@@ -19,7 +19,7 @@ namespace DataObjects
 
         private string Category_ID;
 
-        public int? ProductId { get; set; }
+        public int ProductId { get; set; }
 
         public byte[] Image_Binary { get; set; }
 
