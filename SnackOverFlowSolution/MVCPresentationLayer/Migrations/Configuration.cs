cc0b15bf40e45bc8a1b901bdabdb1a6550798fee
Christian <christian.lopez.home@gmail.com>
2017-04-14 11:21:16 -0500

    CL Added Roles to EF, modified Apply for Supplier page to not let people go if they are already supplier/applied

--- SnackOverFlowSolution/MVCPresentationLayer/Migrations/Configuration.cs
+++ SnackOverFlowSolution/MVCPresentationLayer/Migrations/Configuration.cs
@@ -7,6 +7,7 @@ namespace MVCPresentationLayer.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Security.Claims;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MVCPresentationLayer.Models.ApplicationDbContext>
     {
@@ -15,10 +16,52 @@ namespace MVCPresentationLayer.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+        /// <summary>
+        /// Christian Lopez
+        /// 2017/04/14
+        /// 
+        /// Adding Admin and other roles - Mimic code used in .NET class
+        /// </summary>
+        /// <remarks>William Flood enabled Migrations 2017/04/13</remarks>
+        /// <param name="context"></param>
         protected override void Seed(MVCPresentationLayer.Models.ApplicationDbContext context)
         {
             var userStore = new UserStore<ApplicationUser>(context);
             var userManager = new UserManager<ApplicationUser>(userStore);
+
+            // check for an admin, and if not there, create one
+            if (!context.Users.Any(u => u.UserName == "admin@test.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@test.com",
+                    Email = "admin@test.com"
+                    //FirstName = "System",
+                    //LastName = "Admin"
+                };
+
+                IdentityResult result = userManager.Create(user, "P@ssw0rd");
+
+                if (result.Succeeded)
+                {
+                    // add claims if we want - don't do first one
+                    //userManager.AddClaim(user.Id, new Claim(ClaimTypes.Role, "Administrator"));
+                    userManager.AddClaim(user.Id, new Claim(ClaimTypes.GivenName, "System"));
+                    userManager.AddClaim(user.Id, new Claim(ClaimTypes.Surname, "Administrator"));
+                }
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Administrator" });
+                context.SaveChanges();
+                userManager.AddToRole(user.Id, "Administrator");
+                context.SaveChanges();
+            }
+
+            // Add the roles based on tables in the existing internal system
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Employee" });
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Customer" });
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Supplier" });
+
+            context.SaveChanges();
 
         }
     }
