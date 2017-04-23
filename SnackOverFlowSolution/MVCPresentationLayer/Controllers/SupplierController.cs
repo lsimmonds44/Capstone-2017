89bb78e2f102c9db15576cab8a4ee3de406f620c
Chana001 <sublilung@gmail.com>
2017-04-23 12:52:30 -0500

    Store View Feature and Usability Renforcement

---
+++ SnackOverFlowSolution/MVCPresentationLayer/Controllers/SupplierController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPresentationLayer.Controllers
+{
+    /// <summary>
+    /// Created by Natacha Ilunga
+    /// 4/23/17
+    /// 
+    /// Main Menu for Supplier en relation Fonction
+    /// </summary>
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
