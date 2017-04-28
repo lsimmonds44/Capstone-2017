66c6a176fc7f12dc6b6962370afb1297cad23a11
Skyler H <skylerhiscock@gmail.com>
2017-04-28 18:06:30 -0500

    Relaxed Identity password requirements
    AddressLine2 is no longer required

--- SnackOverFlowSolution/MVCPresentationLayer/App_Start/IdentityConfig.cs
+++ SnackOverFlowSolution/MVCPresentationLayer/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace MVCPresentationLayer
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequiredLength = 7,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
