fd63b1b2bb38fac9df6e68fe8c3cdafa340521c8
MikeTak - Michael Takrama <mtakrama@yahoo.com>
2017-05-05 08:12:54 -0500

    Browse Products (sp_retrieve_product_to_customer) update This is to avoid items that have not been priced to show

--- SnackOverFlowDB/CreateSnackOverflowDB.sql
+++ SnackOverFlowDB/CreateSnackOverflowDB.sql
@@ -5243,10 +5243,10 @@ AS
 	BEGIN
 		SELECT pd.product_id, pd.name, pd.description, pgp.grade_id, pgp.price, sp.supplier_id, sp.farm_name, pc.category_id, pd.image_binary
 		FROM Product pd
-		LEFT JOIN Product_Lot pl ON (pl.product_id = pd.product_id)
-		LEFT JOIN Supplier sp ON (sp.supplier_id = pl.supplier_id)
-		LEFT JOIN Product_Grade_Price pgp ON (pgp.product_id = pd.product_id)
-		LEFT JOIN Product_Category pc ON (pc.product_id = pd.product_id)
+		INNER JOIN Product_Lot pl ON (pl.product_id = pd.product_id)
+		INNER JOIN Supplier sp ON (sp.supplier_id = pl.supplier_id)
+		INNER JOIN Product_Grade_Price pgp ON (pgp.product_id = pd.product_id)
+		INNER JOIN Product_Category pc ON (pc.product_id = pd.product_id)
 		WHERE pd.active = 1
 	END
 GO
