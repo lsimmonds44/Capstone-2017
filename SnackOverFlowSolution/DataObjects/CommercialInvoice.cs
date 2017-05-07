d626bac9781b6582321c8b214d4813923814a8d7
Chana001 <sublilung@gmail.com>
2017-05-07 03:14:44 -0500

    Design Splash Ultimate - ViewCart - CommercialInvoice - ApplicationSuccess - ApprovalStatus - Create (Supplier Goods) - Index (Supplier Goods) - Edit (Supplier With Agreements) - Details (Product) - Change Password All navigatable pages have been designed. Kindly notify me if there as some pages which haven't been given navigatable links.

--- SnackOverFlowSolution/DataObjects/CommercialInvoice.cs
+++ SnackOverFlowSolution/DataObjects/CommercialInvoice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +18,33 @@ namespace DataObjects
     /// </summary>
     public class CommercialInvoice
     {
+        [Display(Name="Commercial Invoice ID")]
         public int CommercialInvoiceId { get; set; }
+
+        [Display(Name = "Commercial ID")]
         public int CommercialId { get; set; }
+
+        [Display(Name = "Invoice Date")]
         public DateTime InvoiceDate { get; set; }
+
+        [Display(Name = "Subtotal")]
         public decimal SubTotal { get; set; }
+
+        [Display(Name = "Tax Amount")]
         public decimal TaxAmount { get; set; }
+
+        [Display(Name = "Total")]
         public decimal Total { get; set; }
+
+        [Display(Name = "Amount Paid")]
         public decimal AmountPaid { get; set; }
+
         public bool Approved { get; set; }
+
         public bool Active { get; set; }
+
         public int OrderId { get; set; }
+
         public bool Paid { get; set; }
         
     }
--- SnackOverFlowSolution/DataObjects/SupplierProductLot.cs
+++ SnackOverFlowSolution/DataObjects/SupplierProductLot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataObjects
 {
@@ -11,11 +12,22 @@ namespace DataObjects
     /// </summary>
     public class SupplierProductLot
     {
+        [Display(Name="Supplier Product Lot ID")]
         public int SupplierProductLotId { get; set; }
+
+        [Display(Name="Expiration Date")]
         public DateTime ExpirationDate { get; set; }
+
+        [Display(Name="Product ID")]
         public int ProductId { get; set; }
+
+        [Display(Name="Quantity")]
         public int Quantity { get; set; }
+
+        [Display(Name="Supplier ID")]
         public int SupplierId { get; set; }
+
+        [Display(Name="Price")]
         public decimal? Price { get; set; }
     }
 }
